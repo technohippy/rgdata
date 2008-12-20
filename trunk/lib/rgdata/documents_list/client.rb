bd949b47de4aeeb82cab7c4e5d96ccd699033d2f
andyjpn <andyjpn@8ac42300-cc0a-11dd-86d7-074df07e0730>
2008-12-20 03:11:59 +0000

    refactoring: move some responsibility of token to client

---
+++ trunk/lib/rgdata/documents_list/client.rb
@@ -0,0 +1,12 @@
+require 'rgdata/client'
+
+module RGData
+  module DocumentsList
+    class Client < RGData::Client
+      def upload(file=nil, metadata=nil)
+        
+      end
+    end
+  end
+end
+
--- trunk/lib/rgdata/documents_list/service.rb
+++ trunk/lib/rgdata/documents_list/service.rb
@@ -1,4 +1,5 @@
 require 'rgdata/service'
+require 'rgdata/documents_list/client'
 
 module RGData
   module DocumentsList
@@ -10,8 +11,24 @@ module RGData
         super SERVICE_NAME, SERVICE_URI
       end
 
+      def client_class
+        ::RGData::DocumentsList::Client
+      end
+
       def list_path
         '/feeds/documents/private/full'
+      end
+
+      def metadata(title)
+        # http://code.google.com/intl/en/apis/documents/docs/2.0/developers_guide_protocol.html#UploadingWMetadata
+        %Q{
+<?xml version='1.0' encoding='UTF-8'?>
+<atom:entry xmlns:atom="http://www.w3.org/2005/Atom">
+  <atom:category scheme="http://schemas.google.com/g/2005#kind"
+      term="http://schemas.google.com/docs/2007#document" />
+  <atom:title>#{title}</atom:title>
+</atom:entry>        
+        }
       end
     end
   end
